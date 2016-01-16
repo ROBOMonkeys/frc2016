7f1cf1c35ee39bed5ab3a95f0dbf97408e99f587
Zachary Epps <zactepps@gmail.com>
2016-01-16 11:43:39 -0500

    added preliminary shooting code into Shooter.py

---
+++ Subsystems/Shooter.py
@@ -0,0 +1,10 @@
+from threading import Timer
+
+def shoot(sht_mtrs, piston):
+    sht_mtrs.spin_up() # or, you know, whatever function to start them
+    shoot_timer = Timer(0.5, real_shoot, (sht_mtrs, piston))
+    shoot_timer.start()
+
+def real_shoot(m, p):
+    p.fire() # or whatever function to make the piston fire
+    m.slow_down() # or whatever function to make the motors stop
