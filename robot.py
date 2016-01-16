a73331952b6ee40de9e0aafd4d456059df268706
Darkstorm41 <jason1997.jz@gmail.com>
2016-01-16 11:53:05 -0500

    Init stuff for robot.py

--- robot.py
+++ robot.py
@@ -1,8 +1,14 @@
 import wpilib as wpi
+
 from Subsystems import Lifter, Shooter, Swerve
+
+from enums import XboxAxis, XboxButtons, init_buttons
+
 
 class Myrobot(wpi.IterativeRobot):
     def robotInit(self):
-
+        self.contr = wpi.Joystick(0)
+        init_buttons(self.contr)
+        self.drive = swerve.traindrive              #will fix later
 
 
