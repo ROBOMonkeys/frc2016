854e939a5364b71da6adb361af1ae202b27d27d4
Zachary Epps <zactepps@gmail.com>
2016-01-16 12:43:27 -0500

    added prelim SwerveDrive code

--- Subsystems/Swerve.py
+++ Subsystems/Swerve.py
@@ -1,4 +1,11 @@
-# something something swerve drive LOL
+from wpilib import VictorSP
 
-def drive():
-    # handle the driving okay? plz
+class SwerveDrive():
+    def __init__(self):
+        self.m0 = VictorSP(0)
+        self.m1 = VictorSP(1)
+        self.m2 = VictorSP(2)
+        self.m3 = VictorSP(3)
+
+    def drive(self, speed):
+        # do some drive bullshit here plz
