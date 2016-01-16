4ddea8f8f92aa02a619d565f16e4c48259dd0911
Darkstorm41 <jason1997.jz@gmail.com>
2016-01-16 12:59:56 -0500

    Added TeleopPeriodic stufff

--- robot.py
+++ robot.py
@@ -9,6 +9,24 @@ class Myrobot(wpi.IterativeRobot):
     def robotInit(self):
         self.contr = wpi.Joystick(0)
         init_buttons(self.contr)
-        self.drive = swerve.traindrive              #will fix later
+        self.drive = Swerve.SwerveDrive()
+
+
+    def teleopPeriodic(self):
+        self.y = self.contr.getRawAxis(XboxAxis.R_Y)   # gets the y value from the right stick on the controller
+        self.x = self.contr.getRawAxis(XboxAxis.R_X)   # gets the x value from the right stick on the controller
+        self.rot = self.contr.getRawAxis(XboxAxis.L_X) # gets the x value from the left stick on the controller
+
+        # these if statements allow for a dead area in the controller
+        #  that way the robot won't move by itself when the sticks aren't being touched
+        if self.x < 0.25 and self.x > -0.25:
+            self.x = 0
+        if self.rot < 0.25 and self.rot > -0.25:
+            self.rot = 0
+        if self.y < 0.25 and self.y > -0.25:
+            self.y = 0
+
+        if XboxButtons.A.poll():
+            Shooter.shoot()
 
 
