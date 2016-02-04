8e50e75f906de88256b4c4057c179b7f7f3e3a36
Darkstorm <jason1997.jz@gmail.com>
2016-02-04 13:01:19 -0500

    did stuff

--- util/config.py
+++ util/config.py
@@ -8,6 +8,6 @@ for i in range(4):
     driving_motors.append(VictorSP(i))
     steering_motors.append(VictorSP(i + 4))
 
-encoder = Encoder(1, 2)
+encoder = Encoder(0, 1)
 
 controller = Joystick(0)
