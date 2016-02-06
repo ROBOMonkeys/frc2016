4a73a52bac9bf96c72a22b33ef04aaa466efb3cd
wesles <dogfoodlid21@gmail.com>
2016-02-05 20:57:18 -0500

    open this

---
+++ forzac.py
@@ -0,0 +1,24 @@
+def zac():
+    robot = 12
+    return robot
+
+    print zac()
+
+    zac = true
+    robot = flase
+
+zac = true
+robot = 12
+zac_robot = robot **2
+
+zac = "good person"
+
+zac = "viking"
+zacs_mom = "caregiver"
+
+print zac
+print zacs_mom
+
+zac = "green monkie"
+print len(zac)
+
