13a826dcc18677419ddcc8fd96a9693360fb6e9b
Zac <zactepps@gmail.com>
2016-02-06 12:00:26 -0500

    Update config.py

--- util/config.py
+++ util/config.py
@@ -2,12 +2,15 @@ from wpilib import Encoder, VictorSP, Joystick
 
 driving_motors = []
 steering_motors = []
+encoders = [Encoder(0, 1),
+            Encoder(2, 3),
+            Encoder(4, 5),
+            Encoder(6, 7)]
 
 # driving motors and steering motors setup
 for i in range(4):
     driving_motors.append(VictorSP(i))
     steering_motors.append(VictorSP(i + 4))
-
-encoder = Encoder(0, 1)
+    encoders[i].reset()
 
 controller = Joystick(0)
