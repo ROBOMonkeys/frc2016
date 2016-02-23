53b8bd7e0137510755507389e1292d1c57baf809
Zachary Epps <zactepps@gmail.com>
2016-02-22 19:37:33 -0500

    added missing colon

--- Subsystems/Shooter.py
+++ Subsystems/Shooter.py
@@ -9,7 +9,7 @@ def real_shoot(m, p):
     p.set(True) # or whatever function to make the piston fire
     m.set(0) # or whatever function to make the motors stop
 
-def suck(sht_mtrs, suck_mtrs, piston)
+def suck(sht_mtrs, suck_mtrs, piston):
     sht_mtrs.set(.8)
     suck_mtrs.set(.8)
     piston.set(False)
