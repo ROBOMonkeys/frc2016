a7b89e2189c8806654934bc66bdd0439e2dcc7c7
Zachary Epps <zactepps@gmail.com>
2016-03-04 20:22:01 -0500

    team zac update 3: the return of the accident

--- robot.py
+++ robot.py
@@ -9,6 +9,13 @@ from threading import Timer
 class Myrobot(wpi.IterativeRobot):
     def robotInit(self):
         logging.write_message("""
+mmmmmm   mm     mmm 
+    #"   ##   m"   "
+  m#    #  #  #     
+ m"     #mm#  #     
+##mmmm #    #  "mmm"
+""")
+        logging.write_message("""
                                                                
        mmmmmmm mmmmmm   mm   m    m        
  m"\""m    #    #        ##   ##  ##        
@@ -17,13 +24,6 @@ class Myrobot(wpi.IterativeRobot):
   \"""     #    #mmmmm #    # #    #        
 
         """)
-        logging.write_message("""
-mmmmmm   mm     mmm 
-    #"   ##   m"   "
-  m#    #  #  #     
- m"     #mm#  #     
-##mmmm #    #  "mmm"
-""")
         
         self.drive = Drive.RobotDrive()
         self.drive_type = config.SWERVE
